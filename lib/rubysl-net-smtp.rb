e49bcd6f96332ee90853461624a703540b55a45d
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:51 -0700

    Created gem.

---
+++ lib/rubysl-net-smtp.rb
@@ -0,0 +1,9 @@
+require "rubysl-net-smtp/version"
+
+module RubySL
+  module Net
+    module Smtp
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubysl-net-smtp/version.rb
@@ -0,0 +1,7 @@
+module RubySL
+  module Net
+    module Smtp
+      VERSION = "0.0.1"
+    end
+  end
+end
